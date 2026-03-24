06905e3210fd2ca22aa2a51266fb10b2a420d267
Sofia <Sofia.leivapires@gmail.com>
2026-03-24 14:58:47 -0300

    feat versao inicial sistema bancario

---
+++ poo02/Entidades/ContaBancaria.cs
@@ -0,0 +1,71 @@
+using System.Runtime.CompilerServices;
+
+namespace poo02.Entidades;
+
+public class ContaBancaria
+{
+
+    #region Construtores
+
+    public ContaBancaria(string numero, string titular, string cpf)
+    {
+        if(string.IsNullOrWhiteSpace(numero))
+        {
+            throw new ArgumentException("Número inválido.");
+        }
+        if(string.IsNullOrWhiteSpace(titular))
+        {
+            throw new ArgumentException("Titular inválido.");
+        }
+        if(string.IsNullOrWhiteSpace(cpf))
+        {
+            throw new ArgumentException("CPF inválido.");
+        }
+
+        _numero = numero;
+        _titular = titular;
+        _cpf = cpf;
+        _saldo = 0;
+    }
+
+
+    #endregion
+
+
+
+
+    #region Propriedades Privadas
+
+    private decimal _saldo;
+    private string _numero;
+
+    private string _titular;
+
+    private string _cpf;
+
+    #endregion
+
+    #region Propriedades Públicas
+
+
+    public string Numero
+    {
+        get { return _numero; }
+    }
+
+    public string Titular
+    {
+        get { return _titular; }
+    }
+
+    public string Cpf
+    {
+        get { return _cpf; }
+    }
+
+      public decimal Saldo
+    {
+        get { return _saldo;}
+    }
+    #endregion
+}
---
+++ poo02/Program.cs
@@ -0,0 +1,103 @@
+﻿// Instancia de uma propriedade
+using System.Runtime.ExceptionServices;
+using poo02.Entidades;
+
+
+// Instancia de uma lista de contas bancárias
+List <ContaBancaria> contas = new();
+
+// Variável boolean para controlar a execução do programa
+bool executando = true;
+
+while (executando)
+{
+    Console.Clear();
+    Console.WriteLine("=== Sistema Bancário ===");
+    Console.WriteLine("1. Criar conta");
+    Console.WriteLine("2. Listar contas");
+    Console.WriteLine("0. Sair");
+    Console.Write("\nEscolha");
+
+
+    var opcao = Console.ReadLine();
+
+    try
+    {
+        switch (opcao)
+        {
+            case "1":
+                CriarContas();  
+                break;
+            case "2":
+                ListarContas();  
+                break;
+            case "0":
+                executando = false;
+                break;
+            default:
+                Console.WriteLine("Opção inválida. Pressione Enter para continuar.");
+                Pausar();
+                break;
+        }
+    } catch (Exception ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}");
+        
+    }
+
+
+}
+
+
+#region Métodos
+void ListarContas()
+{
+    Console.Clear();
+    Console.WriteLine("\n ####CONTAS####\n");
+
+    Console.WriteLine("--------------------------------------------------");
+    Console.WriteLine($"{"índice",6} | {"Número", -10} | {"Titular", -40} | {"CPF", -14} | { "Saldo", 15}");
+    Console.WriteLine("--------------------------------------------------");
+
+    for(int i = 0; i < contas.Count; i++)
+    {
+        var conta = contas[i];
+        Console.WriteLine($"| {i,6} | {conta.Numero, -10} | {conta.Titular, -40} | {conta.Cpf, -14} | {conta.Saldo, 15:C}");
+    }
+
+    Console.WriteLine("--------------------------------------------------");
+    Console.WriteLine();
+    Pausar();
+}
+
+void CriarContas()
+{
+    Console.Clear();
+    Console.WriteLine("=== Criar Conta ===");
+
+    Console.Write("Número da conta: ");
+    var numero = Console.ReadLine();
+
+    Console.Write("Titular da conta: ");
+    var titular = Console.ReadLine();
+
+    Console.Write("CPF do titular: ");
+    var cpf = Console.ReadLine();
+
+    // Não entendi por que da erro sem exclamação
+    var conta = new ContaBancaria(numero!, titular!.ToUpper(), cpf!);
+
+    contas.Add(conta);
+
+    Console.WriteLine("Conta criada com sucesso!");
+    Pausar();
+}
+
+void Pausar()
+{
+    Console.WriteLine("Pressione Enter para continuar...");
+    Console.ReadLine();
+}
+
+
+#endregion
